f94057de6e9f542e457bcb7cfe398ce1bfb4cdf8
onebyte2016 <onebyte2016@gmail.com>
2024-05-27 20:18:38 -0700

    backend

--- 0x00-ES6_basic/3-main.js
+++ 0x00-ES6_basic/3-main.js
@@ -1,8 +1,15 @@
-console.log(getSumOfHoods(34));
-console.log(getSumOfHoods(34, 3));
-console.log(getSumOfHoods(34, 3, 4));
-bob@dylan:~$
-bob@dylan:~$ npm run dev 3-main.js 
-142
-56
-41
+import createEmployeesObject from "./11-createEmployeesObject.js";
+import createReportObject from './12-createReportObject.js';
+import createIteratorObject from './100-createIteratorObject.js';
+import iterateThroughObject from './101-iterateThroughObject.js';
+
+
+const employees = {
+    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+    ...createEmployeesObject('marketing', ['Sylvie'])
+};
+
+const report = createReportObject(employees);
+const reportWithIterator = createIteratorObject(report);
+
+console.log(iterateThroughObject(reportWithIterator));
