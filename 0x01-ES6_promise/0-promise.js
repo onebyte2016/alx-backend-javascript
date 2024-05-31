999e52d43657162f9e41755e8957924e7d5c0a57
onebyte2016 <onebyte2016@gmail.com>
2024-05-31 05:14:42 -0700

    javasript classes

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,3 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => 
-    const success = true;
-
-    if (success) {
-      resolve("Success: Data received from API");
-    } else {
-      reject("Error: Failed to fetch data from API");
-    }
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
