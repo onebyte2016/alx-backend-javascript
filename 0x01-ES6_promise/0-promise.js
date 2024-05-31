752834b2303c2a22e0c5fd206e0b9b45a9eaa8b5
onebyte2016 <onebyte2016@gmail.com>
2024-05-31 04:51:38 -0700

    promise task

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => 
+    const success = true;
+
+    if (success) {
+      resolve("Success: Data received from API");
+    } else {
+      reject("Error: Failed to fetch data from API");
+    }
+  });
+}
