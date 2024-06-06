a9ef5ffac790493572a2b42f5eab139a94077050
onebyte2016 <onebyte2016@gmail.com>
2024-06-06 09:26:35 -0700

    add float

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""function add that takes a float a and a float b 
- as arguments and returns their sum as a float.
-"""
+'''Task 0's module.
+'''
 
-def add(a: float, b: float)->float:
-    """ Adds two floating poit numbers
-    """
-    return a + b
+
+def add(a: float, b: float) -> float:
+    '''Adds two floating-point numbers.
+    '''
+    return a + b
